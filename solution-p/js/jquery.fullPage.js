940610d06fa2b025ce1c7c385dfdb554d8e134b1
Evgeny Osminov <fasc1ster@gmail.com>
2016-11-22 01:09:45 +0300

    make final structure with fullpage.js

--- solution-p/js/jquery.fullPage.js
+++ solution-p/js/jquery.fullPage.js
@@ -111,10 +111,10 @@
         // Create some defaults, extending them with any options that were provided
         options = $.extend({
             //navigation
-            menu: false,
+            menu: true,
             anchors:[],
             lockAnchors: false,
-            navigation: false,
+            navigation: true,
             navigationPosition: 'right',
             navigationTooltips: [],
             showActiveTooltip: false,
@@ -125,7 +125,7 @@
 
             //scrolling
             css3: true,
-            scrollingSpeed: 700,
+            scrollingSpeed: 750,
             autoScrolling: true,
             fitToSection: true,
             fitToSectionDelay: 1000,
